788ac6118698625e8b73aafab192a0df1013460e
Fachry <kucingmelayang@gmail.com>
2020-04-22 21:51:14 +0700

    Update paket

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const express = require('express');
+const helmet = require('helmet');
 const socket = require('socket.io');
 const { resolve } = require('path');
 
@@ -8,8 +9,11 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', resolve('./frontend/views'));
+app.use(helmet())
+app.use(helmet.noCache())
+app.use(helmet.referrerPolicy({ policy: ['no-referrer', 'same-origin'] }))
 
 const server = http.createServer(app);
 const io = socket(server);
 
-server.listen(port, _ => console.log(`Listening on port ${port}`));
+server.listen(port, _ => console.log(`Listening on port: ${port}`));
