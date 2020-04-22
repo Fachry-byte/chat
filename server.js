b1966e500588d3cc6619acd34fc5a5b47ec9639c
Fachry <kucingmelayang@gmail.com>
2020-04-22 22:24:45 +0700

    Biasa

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const express = require('express');
+const helmet = require('helmet')
+const socket = require('socket.io');
+const { resolve } = require('path');
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', resolve('./frontend/views'));
+app.use(helmet())
+app.use(helmet.noCache())
+app.use(helmet.referrerPolicy())
+
+const server = http.createServer(app);
+const io = socket(server);
+
+server.listen(port, _ => console.log(`Listening on port ${port}`));
