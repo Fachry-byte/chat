04c6ae2a598868b5acb973321f72c6ecd027aab4
Fachry <kucingmelayang@gmail.com>
2020-04-25 22:19:18 +0700

    Captcha.js

--- frontend/static/js/captcha.js
+++ frontend/static/js/captcha.js
@@ -1,13 +1,31 @@
 function main() {
-    const el = document.getElementById('captcha'), len = el.getAttribute('data-captcha')
-    captcha(el, len)
+    const el = document.getElementById('captcha'), inp = document.getElementById('captcha-input'), len = el.getAttribute('data-captcha');
+    const c = captcha(el, len), btn = document.querySelector('[type="submit"]'), form = document.getElementById('form-c'), res = document.getElementById('captcha-res')
+    discopypaste(el);
+    btn.onclick = _ => {
+        if(inp.value != c || inp.value == '') {
+            sendform(form, false);
+            return res.innerHTML = 'Salah';
+        }
+        return sendform(form, true);
+    }
 }
 
 function captcha(el, len) {
-    const dat = '01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split(''), res = []
-    const rand = n => Math.floor(Math.random() * (n + 1))
-    for (let i = 0; i <= len; i++) res.push(dat[rand(dat.length)])
-    return el.innerHTML = res.join('')
+    const dat = '01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split(''), res = [];
+    const rand = n => Math.floor(Math.random() * n);
+    for (let i = 1; i <= len; i++) res.push(dat[rand(dat.length)]);
+    el.innerHTML = res.join('');
+    return res.join('');
 }
 
-document.addEventListener('DOMContentLoaded', main)
+function sendform(element, submit) {
+    return submit == true ? element.onsubmit = () => { return true } : element.onsubmit = () => { return false }
+}
+  
+function discopypaste(el) {
+    el.oncontextmenu = () => false;
+    return false;
+}
+
+document.addEventListener('DOMContentLoaded', main);
