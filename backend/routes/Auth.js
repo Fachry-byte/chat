efecb096dd1276eba0cbfffb8d4deead9dcd4568
Fachry-byte <kucingmelayang07@gmail.com>
2020-04-29 15:51:42 +0700

    Captcah.js pindah belakang, penamabahan jQuery, dan styling form

--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -5,12 +5,14 @@ const bcrypt = require('bcryptjs');
 
 const { init, checknotAuth } = require('../utils/passport-api');
 const { Auth: db } = require('../data');
-
+const { Captcha, checkCaptcha } = require('../utils/captcha');
+const captcha = new Captcha({ len: 10, field: 'captcha' });
+ 
 init(passport, db);
 
 router.get('/', (req, res) => res.redirect(req.baseUrl + '/masuk'));
 
-router.get('/daftar', checknotAuth, (req, res) => res.render('Auth/daftar', { baseUrl: req.baseUrl, respon: req.flash('respon') }));
+router.get('/daftar', checknotAuth, (req, res) => res.render('Auth/daftar', { baseUrl: req.baseUrl, respon: req.flash('respon'), captcha: captcha.generate() }));
 
 router.get('/masuk', checknotAuth, (req, res) => res.render('Auth/masuk', { baseUrl: req.baseUrl }));
 
@@ -25,10 +27,11 @@ router.delete('/logout', (req, res) => {
 	req.logOut();
 });
 
-router.post('/daftar', checknotAuth, async (req, res) => {
+router.post('/daftar', checknotAuth, checkCaptcha, async (req, res) => {
 	try {
-		const { nama, pw } = req.body;
+		const { nama, pw, captcha } = req.body;
 		const isUserExist = async nama => await db.findOne({ nama }) != null ? true : false
+		console.log(captcha)
 		if (await isUserExist(nama)) {
 			req.flash('respon', 'Email telah terdaftar!');
 			return res.redirect(req.baseUrl + '/daftar');
